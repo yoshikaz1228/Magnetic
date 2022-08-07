2b1a7e2e52059f53d48c5f50eac1d31ff22092ee
yoshikaz <yoshikaz.matsubara@icloud.com>
2022-08-07 23:59:15 +0900

    init ExampleSwiftUI

---
+++ ExampleSwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ExampleSwiftUI
+//
+//  Created by Yoshikaz Matsubara on 2022/08/07.
+//  Copyright © 2022 efremidze. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleSwiftUI/ExampleSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleSwiftUIApp.swift
+//  ExampleSwiftUI
+//
+//  Created by Yoshikaz Matsubara on 2022/08/07.
+//  Copyright © 2022 efremidze. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
